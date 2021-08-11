9857c337f7235d408e8500a4de47582dc90e4c52
snork-maiden <pollykoldysheva@gmail.com>
2021-08-11 16:56:21 +0300

    remove comments, reformat code

--- createKeyboard.js
+++ createKeyboard.js
@@ -1,12 +1,10 @@
 const Markup = require('node-vk-bot-api/lib/markup');
 const colorEnum = {
-    RED : 'negative',
-    GREEN : 'positive',
-    BLUE : 'primary',
-    WHITE : 'secondary' 
+    RED: 'negative',
+    GREEN: 'positive',
+    BLUE: 'primary',
+    WHITE: 'secondary'
 }
-
-
 
 const keyboards = {
     mainMenu: [
@@ -34,58 +32,17 @@ const keyboards = {
         ['Нет, не то', colorEnum.RED],
     ],
 }
-// const keyboards = {
-//     mainMenu: createKeyboard([
-//         ['Написать пост', colorEnum.GREEN],
-//         ['Пост отклонили', colorEnum.RED],
-//         ['Связь с админками', colorEnum.WHITE],
-//     ]),
-//     confirm: createKeyboard([
-//         ['да', colorEnum.GREEN],
-//         ['нет', colorEnum.RED],
-//     ]),
-//     toMainMenu: createKeyboard([
-//         ['Главное меню', colorEnum.BLUE],
-//     ]),
-//     yourDevice: createKeyboard([
-//         ['мобильный', colorEnum.BLUE],
-//         ['компьютер', colorEnum.BLUE],
-//     ]),
-//     themes: createKeyboard([
-//         ['сбор денег', colorEnum.BLUE],
-//         ['животные', colorEnum.BLUE],
-//     ]),
-//     roolsConfirm: createKeyboard([
-//         ['Да, дело в этом!', colorEnum.GREEN],
-//         ['Нет, не то', colorEnum.RED],
-//     ]),
-// }
 
-
-function createKeyboard (buttons) {
+function createKeyboard(buttons) {
     let keyboard = [];
     for (let button of buttons) {
         keyboard.push(
             Markup.button(button[0], button[1])
         );
-        // keyboard.push( createButton(button[0], button[1]) );
     }
-
     return Markup.keyboard(keyboard, { columns: 1 })
-    .inline();
+        .inline();
 }
-
-//  function createButton(label, color) {
-//     return {
-//         action: {
-//             type: 'callback',
-//             label: label,
-//             payload: `{"label": \"${label}\"}`,
-//           },
-//           color: color
-//     }
-//     }
-
 
 module.exports.keyboards = keyboards;
 module.exports.createKeyboard = createKeyboard;
--- script.js
+++ script.js
@@ -1,27 +1,25 @@
 const VkBot = require('node-vk-bot-api');
-const Markup = require('node-vk-bot-api/lib/markup');
 const Session = require('node-vk-bot-api/lib/session');
 const Stage = require('node-vk-bot-api/lib/stage');
 const Scene = require('node-vk-bot-api/lib/scene');
-
-
-const {keyboards: keyboards, createKeyboard: createKeyboard} = require('./createKeyboard.js');
+const { keyboards: keyboards, createKeyboard: createKeyboard } = require('./createKeyboard.js');
 const messageText = require('./messageText.js');
 
 const bot = new VkBot({
-    token:'***REMOVED***'
+  token: '***REMOVED***'
 });
 const session = new Session();
+
 const scene = new Scene('meet',
   (ctx) => {
     ctx.scene.next();
     ctx.reply(messageText.mainMenu, null, createKeyboard(keyboards.mainMenu));
   },
   (ctx) => {
-      ctx.scene.next();
-      if ( ctx.message.text == 'Написать пост') {
-          ctx.reply(messageText.createPost.askUser, null, keyboards.confirm);
-      }
+    ctx.scene.next();
+    if (ctx.message.text == 'Написать пост') {
+      ctx.reply(messageText.createPost.askUser, null, keyboards.confirm);
+    }
   },
   (ctx) => {
     ctx.session.name = ctx.message.text;
@@ -29,6 +27,7 @@ const scene = new Scene('meet',
     ctx.scene.leave();
     ctx.reply(`Nice to meet you, ${ctx.session.name} (${ctx.session.age} years old)`);
   });
+
 const stage = new Stage(scene);
 
 bot.use(session.middleware());
@@ -39,54 +38,20 @@ bot.on((ctx) => {
 });
 
 bot.use(async (ctx, next) => {
-    try {
-      await next();
-    } catch (e) {
-      console.error(e);
-    }
-  });
-
-  function mainMenu(ctx){
-      VkBotClientInfo.keyboard ? mainMenuButtons(ctx) : mainMenuNumbers(ctx);
+  try {
+    await next();
+  } catch (e) {
+    console.error(e);
   }
-  
+});
 
-/*   bot.command('start', (ctx) => {
-    ctx.reply('Выберите, шо надо', null, Markup
-      .keyboard([
-          {
-              action: {
-                type: 'text',
-                label: 'Написать пост',
-              },
-              color: 'positive'
-            },
-          {
-              action: {
-                type: 'text',
-                label: 'Пост отклонили',
-              },
-              color: 'negative'
-            },
-        'Связь с админками',
-      ], { columns: 1 })
-      .inline(),
-    );
-  });
-
-  bot.on((ctx) => {
-    if(ctx.message.text == 'Написать пост') {
-        
-    } else if(ctx.message.text == 'Связь с админками') {
-
-    } else {
-        ctx.reply(`Нажми на кнопку - получишь результат.`);
-      }
-  }); */
+function mainMenu(ctx) {
+  return VkBotClientInfo.keyboard ? mainMenuButtons(ctx) : mainMenuNumbers(ctx);
+}
 
 bot.startPolling((err) => {
-    if (err) {
-      console.error(err);
-    }
-    console.log('bot started');
-  });
+  if (err) {
+    console.error(err);
+  }
+  console.log('bot started');
+});
