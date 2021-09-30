4939af4fe80c140e793ff168fd0e6fedc66b5524
Offout <offout1@gmail.com>
2021-09-30 16:52:15 +0200

    Dockerize, use prod images, add more logging

--- messageImg.js
+++ messageImg.js
@@ -1,12 +1,12 @@
 //for tmh
-// const messageImgDesktop = ["photo-64474568_457328474", "photo-64474568_457328475", "photo-64474568_457328476", "photo-64474568_457328477"];
+const messageImgDesktop = ["photo-64474568_457328474", "photo-64474568_457328475", "photo-64474568_457328476", "photo-64474568_457328477"];
  
-// const messageImgMobile = ["photo-64474568_457328478", "photo-64474568_457328479", "photo-64474568_457328480", "photo-64474568_457328481", "photo-64474568_457328482"];
+const messageImgMobile = ["photo-64474568_457328478", "photo-64474568_457328479", "photo-64474568_457328480", "photo-64474568_457328481", "photo-64474568_457328482"];
 
 //for temporary public group
-const messageImgDesktop = ["photo-189592663_457239019", "photo-189592663_457239020", "photo-189592663_457239021", "photo-189592663_457239022"];
+// const messageImgDesktop = ["photo-189592663_457239019", "photo-189592663_457239020", "photo-189592663_457239021", "photo-189592663_457239022"];
  
-const messageImgMobile = ["photo-189592663_457239023", "photo-189592663_457239024", "photo-189592663_457239025", "photo-189592663_457239026", "photo-189592663_457239027"];
+// const messageImgMobile = ["photo-189592663_457239023", "photo-189592663_457239024", "photo-189592663_457239025", "photo-189592663_457239026", "photo-189592663_457239027"];
 
 module.exports.messageImgDesktop = messageImgDesktop;
 module.exports.messageImgMobile = messageImgMobile;
